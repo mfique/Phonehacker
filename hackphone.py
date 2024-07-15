82adf8b703a105bd17e89dca8c8b93af9bdedeb4
mfique <pacifiquem58@gmail.com>
2024-07-15 13:03:04 +0200

    Mobile telephone hacking

---
+++ hackphone.py
@@ -0,0 +1,12 @@
+import phonenumbers
+from phonenumbers import geocoder
+phone_number1 = phonenumbers.parse("+250781202570")
+phone_number2 = phonenumbers.parse("+201234567890")
+phone_number3 = phonenumbers.parse("+12136574429")
+phone_number4 = phonenumbers.parse("+917294536271")
+
+print("\nPhone Numbers Location\n")
+print(geocoder.description_for_number(phone_number1, "en"));
+print(geocoder.description_for_number(phone_number2, "en"));
+print(geocoder.description_for_number(phone_number3, "en"));
+print(geocoder.description_for_number(phone_number4, "en"));
